1569d78940eeedfeb3f3ed79dc101f7b060a2a7a
martin <audumart@gmail.com>
2017-07-03 11:39:59 +0100

    adding basic functionalities to admin home page

---
+++ www/adminhome.php
@@ -0,0 +1,31 @@
+<?php
+	
+	session_start();
+	$page_title = "Home";
+
+	include 'includes/db.php';
+
+	include 'includes/functions.php';
+
+	include 'includes/view.php';
+
+
+
+
+
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home</title>
+</head>
+<body>
+
+		
+
+		
+	
+
+</body>
+</html>
