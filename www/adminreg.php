035e1c435ac29b635ce55d9c8ada38d8e8b62b0d
martin <audumart@gmail.com>
2017-07-03 13:05:11 +0100

    adding css files to registration page

--- www/adminreg.php
+++ www/adminreg.php
@@ -74,61 +74,92 @@
 }
 
 ?>
+<!doctype html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang=""> <!--<![endif]-->
+
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>Register</title>
+        <link rel="stylesheet" href="../css/regstyle.css" type="text/css" />
+</head>
+  <div class="container">
+  <!--<img src="../images/projectXE.png" alt="logo" width="20%" height="5%">-->
+</div>
+
+
+<div class="container1">
 <div class="wrapper">
-		<h1 id="register-label">Admin Register</h1>
-		<hr>
-		<form id="register"  action ="adminreg.php" method ="POST">
-			<div>
-			<?php
-			$display = displayErrors($errors,'fname');
-			echo $display;
-			?>
-				<label>first name:</label>
-				<input type="text" name="fname" placeholder="first name">
-			</div>
-			<div>
-			<?php
-			$display = displayErrors($errors,'lname');
-			echo $display;
-			?>
-				<label>last name:</label>	
-				<input type="text" name="lname" placeholder="last name">
-			</div>
+    <h1 id="register-label">Admin Register</h1>
+    <hr>
+    <form id="register"  action ="adminlogin.php" method ="POST">
+      <div>
+      <?php
+      $display = displayErrors($errors,'fname');
+      echo $display;
+      ?>
+        
+        <input type="text" name="fname" placeholder="first name" required>
+      </div>
+      <div>
+      <?php
+      $display = displayErrors($errors,'lname');
+      echo $display;
+      ?>
+        
+        <input type="text" name="lname" placeholder="last name" required>
+      </div>
 
-			<?php
-			$display = displayErrors($errors,'uname');
-			echo $display;
-			?>
-				<label>last name:</label>	
-				<input type="text" name="uname" placeholder="username">
-			</div>
+      <?php
+      $display = displayErrors($errors,'uname');
+      echo $display;
+      ?>
+        
+        <input type="text" name="uname" placeholder="username" required>
+      </div>
 
-			<div>
-			<?php
-			$display = displayErrors($errors,'email');
-			echo $display;
-			?>
-				<label>email:</label>
-				<input type="text" name="email" placeholder="email">
-			</div>
-			<div>
-			<?php
-			$display = displayErrors($errors,'password');
-			echo $display;
-			?>
-				<label>password:</label>
-				<input type="password" name="password" placeholder="password">
-			</div>
+      <div>
+      <?php
+      $display = displayErrors($errors,'email');
+      echo $display;
+      ?>
+       
+        <input type="text" name="email" placeholder="email" required>
+      </div>
+      <div>
+      <?php
+      $display = displayErrors($errors,'password');
+      echo $display;
+      ?>
+       
+        <input type="password" name="password" placeholder="password" required>
+      </div>
  
-			<div><?php
-			if (isset($errors['pword'])) { echo '<span class="err">'.$errors['pword'].'</span>'; }
-			?>
-				<label>confirm password:</label>	
-				<input type="password" name="pword" placeholder="password">
-			</div>
+      <div><?php
+      if (isset($errors['pword'])) { echo '<span class="err">'.$errors['pword'].'</span>'; }
+      ?>
+         
+        <input type="password" name="pword" placeholder="confirm password" required>
+      </div>
 
-			<input type="submit" name="register" value="register">
-		</form>
+      <button type="submit" class="registerbtn" name="register" value="register">Register</button>
+    </form>
 
-		<h4 class="jumpto">Have an account? <a href="adminlogin.php">login</a></h4>
-	</div>
+     
+
+    
+     
+      
+    
+  
+
+    <h4 class="jumpto">Have an account? <a href="adminlogin.php">login</a></h4>
+
+    </div>
+
+  </div>
+</html>
