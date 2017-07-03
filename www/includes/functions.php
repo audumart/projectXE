5435dc6f320b95586195f67152e908de80afc78f
martin <audumart@gmail.com>
2017-07-03 14:45:43 +0100

    adding add members, add services and manage services funtions

--- www/includes/functions.php
+++ www/includes/functions.php
@@ -95,3 +95,66 @@ function doesUsernameExist($dbconn, $uname) {
 	function redirect($loca){
 		header("Location: " .$loca);
 	}
+
+	function addTeamMember($dbconn, $add){
+		define('MAX_FILE_SIZE', '2097152');
+
+		$ext = ["image/jpg", "image/jpeg", "image/png"];
+
+		$rnd = rand(0000000000, 9999999999);
+
+		$strip_name = str_replace(" ", " _ ", $_FILES['member']['name']);
+
+		$filename = $rnd.$strip_name;
+		$destination = 'uploads/'.$filename;
+
+			if (array_key_exists('save', $_POST)) {
+
+				$errors = [];
+
+			
+			if (empty($_FILES['member']['name'])) {
+				$errors[] = "Please choose a file";
+			}
+
+		if ($_FILES['member']['size'] > MAX_FILE_SIZE ) {
+			$errors[] = "file size exceeds maximum. maximum: ". MAX_FILE_SIZE;
+		}
+		if (!in_array($_FILES['member']['type'], $ext)) {
+			$errors[] = "invalid file type";
+		}
+		if (empty($errors)) {
+			if (!move_uploaded_file($_FILES['member']['tmp_name'], $destination)) {
+				$errors[] = "file upload failed";
+			}
+		echo "done";
+		}
+		else{
+			foreach ($errors as $err) {
+				echo $err. '</br>';
+			}
+		}
+	}
+		
+
+		
+
+		$stmt = $dbconn->prepare("INSERT INTO team(member_name, member_number, member_service, member_email, file_path)
+											VALUES(:mn, :mnu, :ms, :me, :fi)");
+		$stmt->bindParam(':mn', $add['member_name']);
+		$stmt->bindParam(':mnu', $add['member_number']);
+		$stmt->bindParam(':mnu', $add['member_service']);
+		$stmt->bindParam(':mnu', $add['member_email']);
+		$stmt->bindParam(':fi',$destination);
+		$stmt->execute();
+		$data = [
+			'mn' => $add['member_name'],
+			':mnu' => $add['member_number'],
+			':ms' => $add['member_service'],
+			':pr' => $add['member_email'],
+			':fi' => $destination
+
+				];
+
+			$stmt->execute($data);
+	}
