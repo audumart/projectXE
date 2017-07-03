d3aa9ec62c8bc93e317e0bb142395280add1ddd5
martin <audumart@gmail.com>
2017-07-03 11:07:21 +0100

    adding function to check if admin username and email exists

--- www/includes/functions.php
+++ www/includes/functions.php
@@ -19,3 +19,52 @@
 
  	$stmt->execute($data);	
 	}
+
+	function doesEmailExist($dbconn, $email) {
+		$result = false;
+
+		$stmt = $dbconn->prepare("SELECT email FROM admin WHERE 	email=:e");
+		# bind params
+		$stmt->bindParam(":e", $email);
+		$stmt->execute();
+
+		# get number of rows returned
+		$count = $stmt->rowCount();
+
+		if ($count > 0) {
+			$result = true;
+		}
+
+		return $result;
+
+	}
+
+function doesUsernameExist($dbconn, $uname) {
+		$result = false;
+
+		$stmt = $dbconn->prepare("SELECT username FROM admin WHERE 	username=:un");
+		# bind params
+		$stmt->bindParam(":un", $uname);
+		$stmt->execute();
+
+		# get number of rows returned
+		$count = $stmt->rowCount();
+
+		if ($count > 0) {
+			$result = true;
+		}
+
+		return $result;
+
+	}
+
+	function displayErrors($open, $name){
+		$result = "";
+
+		if (isset($open[$name])) {
+			
+			$result = '<span class="err">'.$open[$name].'</span>';
+		}
+
+		return $result;
+	}
