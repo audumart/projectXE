bf5186c2f110eea0c0ec38a5f7fe0f115d84012e
martin <audumart@gmail.com>
2017-07-03 20:47:21 +0100

    modifications

--- www/includes/view.php
+++ www/includes/view.php
@@ -1,24 +1,34 @@
 <!DOCTYPE html>
-<html>
-<head>
+
+		<title><?php echo $page_title ?></title>
+		
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                      <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+
+          <div class="brand-bg">
+                    <a class="navbar-brand" href="index.html"><img src="../images/logo.png" alt="projectXE logo" /></a>
+          </div>
+                </div>
+
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul class="nav navbar-nav pull-right">
+                       <li><a href="addmembers.php">Add Team members</a></li>
+                    <li><a href="members.php">View team members</a></li>
+                    <li><a href="services.php">Add services</a></li>
+                    <li><a href="viewservices.php">View services</a></li>
+                    <li><a href="logout.php">Logout</a></li>
+
+                    </ul>
+                </div><!-- /.navbar-collapse -->
+        </div>
+
+            </div><!-- /.container-fluid -->
+        </nav>
 	<title><?php echo $page_title ?></title>
 	
-</head>
-<body>
-	<section>
-		<div class="mast">
-			<h1><a href="adminhome.php">project<span>XE</span> Home </a></h1>
-			<nav>
-				<ul class="clearfix">
-					<li><a href="addmembers.php">Add Team members</a></li>
-					<li><a href="members.php">View team members</a></li>
-					<li><a href="services.php">Add services</a></li>
-					<li><a href="viewservices.php">View services</a></li>
-					<li><a href="logout.php">Logout</a></li>
-				</ul>
-			</nav>
-		</div>
-	</section>
-
-</body>
-</html>
