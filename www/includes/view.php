f043000d7ed4c2865d087343259d3349748d4a5e
martin <audumart@gmail.com>
2017-07-03 11:40:26 +0100

    fixing some typo errors

---
+++ www/includes/view.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title><?php echo $page_title ?></title>
+	
+</head>
+<body>
+	<section>
+		<div class="mast">
+			<h1><a href="adminhome.php">project<span>XE</span> Home </a></h1>
+			<nav>
+				<ul class="clearfix">
+					<li><a href="addmembers.php">Add Team members</a></li>
+					<li><a href="members.php">View team members</a></li>
+					<li><a href="services.php">Add services</a></li>
+					<li><a href="viewservices.php">View services</a></li>
+					<li><a href="logout.php">Logout</a></li>
+				</ul>
+			</nav>
+		</div>
+	</section>
+
+</body>
+</html>
