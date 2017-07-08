cf88d88fcad8b35bb1a8c0ee18f07158d1ad95bf
martin <audumart@gmail.com>
2017-07-08 11:49:43 +0100

    making specific view pages for admin backend management

--- www/includes/view.php
+++ www/includes/view.php
@@ -2,25 +2,101 @@
 <html>
 <head>
     <title><?php echo $page_title ?></title>
-    <link rel="stylesheet" type="text/css" href="../styles/styles.css">
-</head>
-<body>
-    <section>
-        <div class="mast">
-            <h1><a href="adminhome.php">Project<span>XE</span> Home </a></h1>
-            <nav>
-                <ul class="clearfix">
-                    <li><a href="addmembers.php">Add Members</a></li>
-                    <li><a href="members.php">View Members </a></li>
-                    <li><a href="services.php">Add Services</a></li>
-                    <li><a href="viewservices.php">View Services</a></li>
+
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="apple-touch-icon" href="apple-touch-icon.png">
+
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+
+    <!--For font-awesome css-->
+    <link rel="stylesheet" href="../css/font-awesome.min.css" />
+    <link rel="stylesheet" href="../css/montserrat-web-font.css" />
+
+    <!--Theme custom css -->
+    <link rel="stylesheet" href="../css/mainstyle.css">
+
+    <!--Theme Responsive css-->
+    <link rel="stylesheet" href="../css/responsive.css" />
+    <script src="assets/js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+    </head>
+
+    <body>
+    <nav class="mainmenu navbar navbar-default navbar-fixed-top">
+        <div class="container">
+
+    <div class="row">
+                    <!-- Brand and toggle get grouped for better mobile display -->
+                    <div class="navbar-header">
+                            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                                    <span class="sr-only">Toggle navigation</span>
+                                    <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                                    <span class="icon-bar"></span>
+                            </button>
+
+        <div class="brand-bg">
+                            <a class="navbar-brand" href="index.html"><img src="../images/logo.png" alt="projectXE logo" /></a>
+        </div>
+                    </div>
+
+                    <!-- Collect the nav links, forms, and other content for toggling -->
+                    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                            <ul class="nav navbar-nav pull-right">
+
+                     <li><a href="admin.php">Home</a></li>
+                    <li><a href="addmembers.php">Add Team members</a></li>
+                    <li><a href="members.php">View team members</a></li>
+                    <li><a href="viewservices.php">View services</a></li>
                     <li><a href="logout.php">Logout</a></li>
                 </ul>
-            </nav>
-        </div>
-    </section>
+        </div><!-- /.navbar-collapse -->
+</div>
+
+</div><!-- /.container-fluid -->
+</nav>
 
 </body>
+
+<!--Home page style-->
+<header class="home-bg">
+<div class="overlay2-img">
+<div class="container">
+<div class="row">
+    <div class="col-md-6 col-sm-12 col-xs-12">
+        <div class="header-details">
+            <h1>Hello Admin you can manage team services from here<i class="fa fa-circle"></i></h1>
+        </div>
+    </div>
+</div>
+</div>
+</div>
+</header>
+
+<!-- Sections -->
+<section id="promotion-area" class="sections">
+        <div class="container">
+                <!-- Example row of columns -->
+                <div class="row">
+    <div class="col-sm-4 col-sm-12 col-xs-12">
+        <div class="promotion">
+
+        <h1>Work<br>Accuracy<i class="fa fa-circle"></i></h1>
+        <p>Managing your website better</p>
+
+        </div>
+    </div>
+
+    <div class="col-sm-4 col-sm-12 col-xs-12">
+        <div class="promotion">
+
+        <h1>Better<br>Management<i class="fa fa-circle"></i></h1>
+        <p>Don't overtweak this section too much</p>
+
+        </div>
+    </div>
+
+
+    </div>
 </html>
-	
 
